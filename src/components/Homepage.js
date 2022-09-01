0632355aeedb8fdea6c9439b084b542dffd697f9
emremataraci <mtrcemre@gmail.com>
2022-09-01 04:49:06 +0300

    this was first day for out app. We have a long road ahead of us

---
+++ src/components/Homepage.js
@@ -0,0 +1,40 @@
+import React from "react"
+import "./Homepage.css"
+
+
+const Homepage = () => {
+  return (
+    <>
+      <section className='homepage'>
+        <div className='container-1'>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+            <p>There will be news here. </p>
+
+        </div>
+        <div className='container-2'>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        <p>There will be news here. Our back-end developer Ahmet Özkan Şahin will make it. Please calm down.</p>
+        </div>
+      </section>
+      
+
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Homepage;
